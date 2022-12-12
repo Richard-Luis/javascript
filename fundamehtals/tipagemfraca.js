d316ff7c1a5b5768d12a5d2c1b59af27b5ada479
Richard-Luis <gordon.exee@gmail.com>
2022-12-12 20:31:39 -0300

    exercicios

---
+++ fundamehtals/tipagemfraca.js
@@ -0,0 +1,11 @@
+let qualquer = 'legal';
+console.log(qualquer);
+console.log(typeof qualquer);
+
+qualquer = 3.1516;
+console.log(qualquer);
+console.log(typeof qualquer); // avoid generic name and acronyms
+
+let valor = '';
+let numero = 1;
+let pqp = false // Dangerus chemical product... kkk
