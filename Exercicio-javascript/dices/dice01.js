560ad3fc26664548986eba16fd43c9d9b4199ffb
Richard-Luis <gordon.exee@gmail.com>
2022-12-19 20:13:57 -0300

    mudei o nome do let

--- Exercicio-javascript/dices/dice01.js
+++ Exercicio-javascript/dices/dice01.js
@@ -1,11 +1,11 @@
 let name = 'pen';
-let amount = 10;
+let quantity = 10;
 let price = 6.4;
 let tax = 1.5
 let finalprice = price + tax;
 
 console.log(name);
-console.log(amount);
+console.log(quantity);
 console.log(price);
 console.log(tax);
 console.log(finalprice);
