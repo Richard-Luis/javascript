e0cc9606d6a42861f7002ab625e8a340add1c489
Richard-Luis <gordon.exee@gmail.com>
2023-01-26 19:32:22 -0300

    desafio 09

---
+++ pratica-js/exudemy09.js
@@ -0,0 +1,19 @@
+function aluno(nota) {
+    let notaCorrigida = arredondar(nota)
+    if(notaCorrigida >= 40){
+        console.log("Aprovado com: " + notaCorrigida)
+    } else {
+        console.log("Reprovado com: " + notaCorrigida)
+    }
+}
+
+function arredondar(nota){
+    if (nota % 5 > 2){
+        return nota + (5 - (nota % 5))
+    } else {
+        return nota
+    }
+}
+ aluno(41)
+
+ 
