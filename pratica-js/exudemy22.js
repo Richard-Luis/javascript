b5461df13b49e70bcf00520d9d618e5294f70814
Richard-Luis <gordon.exee@gmail.com>
2023-02-08 12:32:33 -0300

    desafio

--- pratica-js/exudemy22.js
+++ pratica-js/exudemy22.js
@@ -1,3 +1,10 @@
-function valorASerPago(mes){
-    
+function valorASerPago(mes, valor){
+    if(mes > 0 && mes < 13) {
+        atraso = mes -1
+        return (valor * ((1 + (5/100)) ** atraso)).toFixed(2)
+    } else {
+        return 'Mês inválido.'
+    }
 }
+
+console.log(valorASerPago(4, 100))
