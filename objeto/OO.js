71eef6864dc56d6d00a4bdb43cf3e834ec81ec1a
Richard-Luis <gordon.exee@gmail.com>
2023-02-08 14:19:23 -0300

    OO ex01

---
+++ objeto/OO.js
@@ -0,0 +1,25 @@
+//CÓDIGO NÃO EXECUTÁVEL!!!
+
+//procedural 
+processamento(valor1, valor2, valor3)
+
+
+// OO
+
+
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento() // foco passou ser o objeto
+ // principios importantes:
+ //1. abstração
+ //2. encapsulamento
+ //3. herança (prototype)
+ //4. poliformismo
