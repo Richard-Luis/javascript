540de0e0246798ff4af011d9cce496978fdcd706
Richard-Luis <gordon.exee@gmail.com>
2023-03-23 22:11:29 -0300

    heranças JS

--- objeto/heranca04.js
+++ objeto/heranca04.js
@@ -23,4 +23,6 @@ obj3.falar()
 
 //Resumo
 console.log((new MeuObjeto).__proto__ === MeuObjeto.prototype)
-console.log(MeuObjeto.__proto__)
+console.log(MeuObjeto.__proto__ === Function.prototype)
+console.log(Function.prototype.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
