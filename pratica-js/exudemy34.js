fefd6f6eb45ef1eae9470fd8f96d6e0e790539cd
Richard-Luis <gordon.exee@gmail.com>
2023-05-29 22:35:14 -0300

    exercicios

---
+++ pratica-js/exudemy34.js
@@ -0,0 +1,10 @@
+function recebndoString(vetor){
+    let string1 = ApoCaLipSe
+    let string2 = Apocalipse
+
+    for (let i = 0; i < vetor.length; i++) {
+        if(string1 &= string2)
+    }
+
+    return
+}
