36e90a00b379d35da40c17a49c87d0aa77f34f1d
Richard-Luis <gordon.exee@gmail.com>
2023-07-04 22:54:11 -0300

    api

--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -2,10 +2,23 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 //get é uma forma de requisição
 app.get('/produtos', (req, res, next) =>{
-    res.send({nome: 'Notebook', preco: 123.54}) // Converter para JSON
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) =>{
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) =>{
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) //vai gerar um JSON
 })
 
 app.listen(porta, () =>{
